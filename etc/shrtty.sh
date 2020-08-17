fe2e2bdba125986e6070c5f089756c2661f5daca
oofnikj <oofnik@gmail.com>
2020-08-17 10:05:56 +0300

    shrtty: share your terminal over TCP

---
+++ etc/shrtty.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+###           shrtty            ###
+#   a simple script to share a    #
+#   terminal session over TCP     #
+###                             ###
+set -e
+
+PORT=${1:-9119}
+
+shrtty() {
+  fifo=$(mktemp -u)
+  mkfifo $fifo
+  nc -kl $PORT <$fifo >/dev/null &
+  printf "listening on :$PORT\n"
+  printf "connect with 'nc $(hostname) $PORT'\n"
+  printf "press ^D to exit\n"
+  PS1='(tc) $PS1'
+  if [[ $(uname) == "Darwin" ]]; then
+    script -qF $fifo
+  else
+    script -qf $fifo
+  fi
+}
+
+cleanup() {
+  kill -PIPE %1
+  rm $fifo
+}
+
+trap cleanup EXIT
+shrtty
